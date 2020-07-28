c56e2cfcef1799c6647382f77463124a6665a0c9
brokenhoax <brokenhoax@outlook.com>
2020-07-28 00:38:28 -0500

    updated css and added clock

---
+++ js.js
@@ -0,0 +1,29 @@
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("datetime").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+    var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
+
+
+document.body.className += ' fade-out'
+
+$(function() {
+  $('body').removeClass('fade-out');
+});
